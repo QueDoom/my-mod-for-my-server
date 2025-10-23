9f571753f98b217c63f47759f4bdfc1f5728c8c1
QueDoom <norton@totl.se>
2025-10-23 19:12:18 +0200

    Initial commit ;)

---
+++ src/main/java/net/quedoom/modden/ModTilLServerClient.java
@@ -0,0 +1,10 @@
+package net.quedoom.modden;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTilLServerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
